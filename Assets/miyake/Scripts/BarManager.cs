2150a2564543307e283b8b756cf2ad938cbbb083
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2024-02-28 16:37:33 +0900

    ミニゲームの作成（途中）

---
+++ Assets/miyake/Scripts/BarManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarManager : MonoBehaviour
+{
+    public GameObject good;
+    public GameObject veryGood;
+    public GameObject Bad;
+    public Slider slider;
+    private bool maxValue;
+    private bool isClicked;
+
+    void Start()
+    {
+        slider.value = 0;
+        maxValue = false;
+        isClicked = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isClicked = true;
+
+            if (slider.value >= 85)
+            {
+                veryGood.SetActive(true);
+            }
+            else if (slider.value >= 50)
+            {
+                good.SetActive(true);
+            }
+
+            else if (slider.value < 50)
+            {
+                Bad.SetActive(true);
+            }
+        }
+
+        //クリックされていなければ実行
+        if (!isClicked)
+        {
+            //最大値に達した場合と、最小値に戻った場合のフラグ切替え
+            if (slider.value == slider.maxValue)
+            {
+                maxValue = true;
+            }
+
+            if (slider.value == slider.minValue)
+            {
+                maxValue = false;
+            }
+
+            //フラグによるスライダー値の増減
+            if (maxValue)
+            {
+                slider.value -= 0.5f;
+            }
+            else
+            {
+                slider.value += 0.5f;
+            }
+        }
+    }
+}
